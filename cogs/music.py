29c241fe929a59a1f4aaf973041fe35920e085ec
tsoumagas-benjamin <bptsoumagas@gmail.com>
2022-05-18 16:10:23 -0400

    Fixes to music cog

--- cogs/music.py
+++ cogs/music.py
@@ -124,7 +124,7 @@ class Music(commands.Cog, name="Music"):
         """Get the bot to leave a voice channel
         
         Example: `$leave`"""
-        await interaction.user.disconnect()
+        await interaction.guild.voice_client.disconnect()
         
     @nextcord.slash_command()
     async def play(self, interaction: Interaction, *, url):
