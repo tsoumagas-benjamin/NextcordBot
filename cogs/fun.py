3dea298c3ae2f5a6d0ce468f0bec819365e56161
tsoumagas-benjamin <bptsoumagas@gmail.com>
2022-06-01 01:01:23 -0400

    Birthday task now runs at midnight

--- cogs/fun.py
+++ cogs/fun.py
@@ -98,7 +98,7 @@ class Fun(commands.Cog, name="Fun"):
         self.daily_joke.cancel()
         self.daily_meme.cancel()
 
-    @tasks.loop(time=datetime.time(5))
+    @tasks.loop(time=datetime.time(4))
     async def daily_birthday(self):
         # Gets daily birthday, if any
         daily_channel = await self.bot.fetch_channel(daily_channel_id)
