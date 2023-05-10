28d2e6b823467f13d3ff86c52507ed3a7081a70d
tsoumagas-benjamin <bptsoumagas@gmail.com>
2023-05-09 21:20:32 -0400

    Cleaning up command to view available commands and their cog

--- cogs/information.py
+++ cogs/information.py
@@ -62,16 +62,18 @@ class Information(commands.Cog, name = "Information"):
     @nextcord.slash_command()
     async def commands(self, interaction: Interaction):
         """Get a list of commands for the bot"""
+        cogs = self.bot.cogs
+        cmd_list = {key: [] for key in cogs}
         commands_list = self.bot.get_application_commands()
-        cmds = []
         for cmd in commands_list:
-            cmds.append(cmd.qualified_name)
-        cmds.sort()
-        bot_commands = ", ".join(cmds)
+            cmd_list[cmd.cog].append(cmd.qualified_name)
         embed = nextcord.Embed(
             title=f"{self.bot.user.name} Commands",
-            description=bot_commands,
             color=nextcord.Colour.from_rgb(214, 60, 26))
+        for cog in cmd_list:
+            cmd_list[cog].sort()
+            cmd_str = ", ".join(cmd_list[cog])
+            embed.add_field(name=f"{cog}", value=f"{cmd_str}", inline=False)
         await interaction.send(embed=embed)  
 
     @nextcord.slash_command()
