f5cc04abf824951418aa1d44fbe7b0dfa60bb667
tsoumagas-benjamin <bptsoumagas@gmail.com>
2024-04-09 02:08:21 -0400

    Removing Reddit API related commands, due to API changes

--- cogs/fun.py
+++ cogs/fun.py
@@ -141,35 +141,6 @@ class Fun(commands.Cog, name="Fun"):
         if daily_channel is None:
             daily_channel = await self.bot.fetch_channel(daily_channel_id)
         await daily_channel.send(embed=joke_task())
-    
-    @tasks.loop(time=datetime.time(0))
-    async def daily_meme(self):
-        # Gets daily meme
-        base_url = f'https://www.reddit.com/r/memes/hot.json'
-        async with ClientSession() as cs:
-            async with cs.get(base_url) as r:
-                res = await r.json(content_type=None)
-                num = random.randint(0, 24)
-                post_data = res['data']['children'][num]['data']
-                post_title = post_data['title']
-                author = post_data['author']
-                post_url = post_data['url']
-                description = post_data['selftext']
-                ups = post_data['ups']
-                ratio = post_data['upvote_ratio']
-                embed = nextcord.Embed(
-                    title=post_title, 
-                    description=description,
-                    color=nextcord.Colour.from_rgb(0, 128, 255))
-                embed.set_image(url=post_url)
-                embed.add_field(
-                    name=f"🔺{ups} upvotes with a {int(ratio*100)}% upvote ratio", 
-                    value=f"Posted by u/{author} [here]({post_url})")
-                daily_channel = self.bot.get_channel(daily_channel_id)
-                if daily_channel is None:
-                    daily_channel = await self.bot.fetch_channel(daily_channel_id)
-                await daily_channel.send(embed=embed)
-                await cs.close()
 
     @nextcord.slash_command()
     async def animal(self, interaction: nextcord.Interaction):
@@ -236,32 +207,6 @@ class Fun(commands.Cog, name="Fun"):
         embed = nextcord.Embed(title='', description=message, color=nextcord.Colour.from_rgb(0, 128, 255))
         embed.set_footer(icon_url=interaction.user.display_avatar,text=f'Requested by {interaction.user.name}')
         await interaction.send(embed=embed)
-    
-    @nextcord.slash_command()
-    async def food(self, interaction: nextcord.Interaction):
-        """Search r/food for a random post"""
-        base_url = f'https://www.reddit.com/r/food/hot.json'
-        async with ClientSession() as cs:
-            async with cs.get(base_url) as r:
-                res = await r.json(content_type=None)
-                num = random.randint(0, 24)
-                post_data = res['data']['children'][num]['data']
-                post_title = post_data['title']
-                author = post_data['author']
-                post_url = post_data['url']
-                description = post_data['selftext']
-                ups = post_data['ups']
-                ratio = post_data['upvote_ratio']
-                embed = nextcord.Embed(
-                    title=post_title, 
-                    description=description,
-                    color=nextcord.Colour.from_rgb(0, 128, 255))
-                embed.set_image(url=post_url)
-                embed.add_field(
-                    name=f"🔺{ups} upvotes with a {int(ratio*100)}% upvote ratio", 
-                    value=f"Posted by u/{author} [here]({post_url})")
-                await interaction.send(embed=embed)
-                await cs.close()
 
     @nextcord.slash_command()
     @application_checks.has_permissions(manage_emojis=True)
@@ -280,33 +225,6 @@ class Fun(commands.Cog, name="Fun"):
                 except nextcord.HTTPException:
                     await interaction.send('The emoji is too big!')
                 await ses.close()
-
-    @nextcord.slash_command()
-    async def getpost(self, interaction: nextcord.Interaction, message: str):
-        """Search a subreddit for a random post"""
-        embed = nextcord.Embed(title='', description='')
-        base_url = f'https://www.reddit.com/r/{message}/hot.json'
-        async with ClientSession() as cs:
-            async with cs.get(base_url) as r:
-                res = await r.json(content_type=None)
-                num = random.randint(0, 24)
-                post_data = res['data']['children'][num]['data']
-                post_title = post_data['title']
-                author = post_data['author']
-                post_url = post_data['url']
-                description = post_data['selftext']
-                ups = post_data['ups']
-                ratio = post_data['upvote_ratio']
-                embed = nextcord.Embed(
-                    title=post_title, 
-                    description=description,
-                    color=nextcord.Colour.from_rgb(0, 128, 255))
-                embed.set_image(url=post_url)
-                embed.add_field(
-                    name=f"🔺{ups} upvotes with a {int(ratio*100)}% upvote ratio", 
-                    value=f"Posted by u/{author} [here]({post_url})")
-                await interaction.send(embed=embed)
-                await cs.close()
     
     @nextcord.slash_command()
     async def guessme(self, interaction: nextcord.Interaction, *, name: str):
@@ -345,33 +263,6 @@ class Fun(commands.Cog, name="Fun"):
         """Gets a random joke from a joke API"""
         result = joke_task()
         await interaction.send(embed=result)
-
-    @nextcord.slash_command()
-    async def meme(self, interaction: nextcord.Interaction):
-        """Gets a random meme from r/memes"""
-        embed = nextcord.Embed(title='', description='')
-        base_url = f'https://www.reddit.com/r/memes/hot.json'
-        async with ClientSession() as cs:
-            async with cs.get(base_url) as r:
-                res = await r.json(content_type=None)
-                num = random.randint(0, 24)
-                post_data = res['data']['children'][num]['data']
-                post_title = post_data['title']
-                author = post_data['author']
-                post_url = post_data['url']
-                description = post_data['selftext']
-                ups = post_data['ups']
-                ratio = post_data['upvote_ratio']
-                embed = nextcord.Embed(
-                    title=post_title, 
-                    description=description,
-                    color=nextcord.Colour.from_rgb(0, 128, 255))
-                embed.set_image(url=post_url)
-                embed.add_field(
-                    name=f"🔺{ups} upvotes with a {int(ratio*100)}% upvote ratio", 
-                    value=f"Posted by u/{author} [here]({post_url})")
-                await interaction.send(embed=embed)
-                await cs.close()
       
     @nextcord.slash_command()
     async def viktor(self, interaction: nextcord.Interaction):
