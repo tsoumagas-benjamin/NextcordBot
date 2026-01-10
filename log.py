75bd7e55362eb8a60191806ee577e9da1788207e
tsoumagas-benjamin <bptsoumagas@gmail.com>
2026-01-09 19:06:42 -0500

    Undoing logging changes to target a timezone

--- log.py
+++ log.py
@@ -1,7 +1,5 @@
 import sys
 import logging
-import pytz
-from datetime import datetime
 
 
 class LogFilter(logging.Filter):
@@ -15,27 +13,6 @@ class LogFilter(logging.Filter):
         return not any(banned_str in record.getMessage() for banned_str in black_list)
 
 
-class Formatter(logging.Formatter):
-    """override logging.Formatter to use an aware datetime object"""
-
-    def converter(self, timestamp):
-        # Create datetime in UTC
-        local_time = datetime.fromtimestamp(timestamp, tz=pytz.UTC)
-        # Change datetime's timezone
-        return local_time.astimezone(pytz.timezone("Canada/Eastern"))
-
-    def formatTime(self, record, datefmt=None):
-        local_time = self.converter(record.created)
-        if datefmt:
-            s = local_time.strftime(datefmt)
-        else:
-            try:
-                s = local_time.isoformat(timespec="milliseconds")
-            except TypeError:
-                s = local_time.isoformat()
-        return s
-
-
 def log():
     # Create logger
     client_logger = logging.getLogger("nextcord.client")
@@ -45,15 +22,13 @@ def log():
     client_logger.addFilter(LogFilter())
     state_logger.addFilter(LogFilter())
 
+    # Format log entries
+    FORMAT = "[{asctime}][{filename}][{lineno:3}][{funcName}][{levelname}] {message}"
+    formatter = logging.Formatter(FORMAT, style="{")
+
     # Create file handler to write to the log
     console_handler = logging.StreamHandler(sys.stdout)
     file_handler = logging.FileHandler("latest.log", mode="w")
-
-    # Format log entries
-    FORMAT = "[{asctime}][{filename}][{lineno:3}][{funcName}][{levelname}] {message}"
-    formatter = file_handler.setFormatter(
-        Formatter(FORMAT, "%Y-%m-%d %H:%M:%S", style="{")
-    )
 
     console_handler.setFormatter(formatter)
     file_handler.setFormatter(formatter)
