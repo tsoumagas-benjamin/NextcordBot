2fd3354079e67480db941f35f240178b688136d7
tsoumagas-benjamin <bptsoumagas@gmail.com>
2026-08-01 11:53:56 -0400

    Updated fun gear to work better async with PostgreSQL and Stoat.py as well as youtube() function

--- gears/fun.py
+++ gears/fun.py
@@ -1,11 +1,12 @@
-import stoat
-from random import choice
-from re import findall
-from stoat.ext import commands
-import urllib.parse as parse
-import urllib.request as request
-from io import BytesIO
+#!/usr/bin/env python
 import asyncio
+from io import BytesIO
+from random import choice
+
+import stoat
+from stoat.ext import commands
+from youtubesearchpython.future import VideosSearch
+
 from utilities import (
     ChaosBot,
     db,
@@ -14,86 +15,6 @@ from utilities import (
     session,
     time_from_string,
 )
-
-# TODO: Switch from MongoDB
-
-
-async def get_advice(client):
-    text_data = await client.get_text("https://api.adviceslip.com/advice")
-    return text_data["slip"]["advice"]
-
-
-async def get_affirmation(client):
-    text_data = await client.get_text("https://www.affirmations.dev/")
-    return text_data["affirmation"]
-
-
-async def get_animal(client):
-    choices = ["birb", "cats", "dogs", "sadcat", "sillycat"]
-    animal_choice = choice(choices)
-    json_data = await client.get_json(f"https://api.alexflipnote.dev/{animal_choice}")
-    return json_data["file"]
-
-
-async def get_joke(client):
-    json_data = await client.get_json(
-        "https://official-joke-api.appspot.com/random_joke"
-    )
-    category = json_data["type"].capitalize()
-    joke = f"{json_data['setup']}\n||{json_data['punchline']}||"
-    return joke, category
-
-
-async def get_meme(client):
-    # Get all info about the meme and take the last/best quality image preview
-    json_data = await client.get_json("https://meme-api.com/gimme")
-    post_title = json_data["title"] if json_data["title"] is not None else ""
-    post_author = json_data["author"]
-    post_subreddit = "r/" + json_data["subreddit"]
-    post_link = json_data["postLink"]
-    nsfw = json_data["nsfw"]
-    spoiler = json_data["spoiler"]
-    ups = json_data["ups"]
-    preview = json_data["preview"][-1]
-
-    description_string = (
-        f"Posted by {post_author} on {post_subreddit} with 🔺{ups} upvotes."
-    )
-
-    # Handling potentially mature/spoiler memes
-    if spoiler is True:
-        preview = f"|| {preview} ||"
-    if nsfw is True:
-        description_string += "\nNSFW content!"
-
-    embed = stoat.SendableEmbed(
-        title=post_title,
-        description=description_string,
-        color="blue",
-        icon_url=preview,
-        url=post_link,
-    )
-
-    return embed
-
-
-# Function to fetch the quote from an API
-async def get_quote(client):
-    text_data = await client.get_text("https://zenquotes.io/api/random")
-    quote = f"*{text_data[0]['q']}*  -  ***{text_data[0]['a']}***"
-    return quote
-
-
-# Function to send content to all the daily channels
-async def send_dailies(bot: commands.Bot, task_embed: stoat.SendableEmbed):
-    # Fetch the list of enrolled channels to post daily content to
-    daily_channels = db.daily_channels.distinct("channel")
-    # Send a meme to each of the daily channels
-    for channel_id in daily_channels:
-        daily_channel = bot.get_channel(channel_id)
-        if daily_channel is None:
-            daily_channel = await bot.fetch_channel(channel_id)
-        await daily_channel.send(embeds=[task_embed])
 
 
 class Fun(commands.Gear, name="Fun"):
@@ -104,7 +25,7 @@ class Fun(commands.Gear, name="Fun"):
     def __init__(self, bot: ChaosBot) -> None:
         self.bot = bot
         # Fetch the list of enrolled warframe channels to post daily content to
-        self.daily_channels = db.daily_channels.distinct("channel")
+        self.daily_channels = self.fetch_daily_channels()
         self.loop = asyncio.get_event_loop()
 
     def gear_load(self):
@@ -113,12 +34,91 @@ class Fun(commands.Gear, name="Fun"):
     def gear_unload(self):
         self.loop.stop(self.daily_fun())
 
+    def fetch_daily_channels(self) -> list[str]:
+        with db.cursor() as cur:
+            cur.execute("SELECT channel_id FROM channels WHERE category = 'daily'")
+            daily_channels = cur.fetchall()
+        return daily_channels
+
+    async def get_advice(self):
+        text_data = await self.bot.client.get_text("https://api.adviceslip.com/advice")
+        return text_data["slip"]["advice"]
+
+    async def get_affirmation(self):
+        text_data = await self.bot.client.get_text("https://www.affirmations.dev/")
+        return text_data["affirmation"]
+
+    async def get_animal(self):
+        choices = ["birb", "cats", "dogs", "sadcat", "sillycat"]
+        animal_choice = choice(choices)
+        json_data = await self.bot.client.get_json(
+            f"https://api.alexflipnote.dev/{animal_choice}"
+        )
+        return json_data["file"]
+
+    async def get_joke(self):
+        json_data = await self.bot.client.get_json(
+            "https://official-joke-api.appspot.com/random_joke"
+        )
+        category = json_data["type"].capitalize()
+        joke = f"{json_data['setup']}\n||{json_data['punchline']}||"
+        return joke, category
+
+    async def get_meme(self):
+        # Get all info about the meme and take the last/best quality image preview
+        json_data = await self.bot.client.get_json("https://meme-api.com/gimme")
+        post_title = json_data["title"] if json_data["title"] is not None else ""
+        post_author = json_data["author"]
+        post_subreddit = "r/" + json_data["subreddit"]
+        post_link = json_data["postLink"]
+        nsfw = json_data["nsfw"]
+        spoiler = json_data["spoiler"]
+        ups = json_data["ups"]
+        preview = json_data["preview"][-1]
+
+        description_string = (
+            f"Posted by {post_author} on {post_subreddit} with 🔺{ups} upvotes."
+        )
+
+        # Handling potentially mature/spoiler memes
+        if spoiler is True:
+            preview = f"|| {preview} ||"
+        if nsfw is True:
+            description_string += "\nNSFW content!"
+
+        embed = stoat.SendableEmbed(
+            title=post_title,
+            description=description_string,
+            color="blue",
+            icon_url=preview,
+            url=post_link,
+        )
+
+        return embed
+
+    # Function to fetch the quote from an API
+    async def get_quote(self):
+        text_data = await self.bot.client.get_text("https://zenquotes.io/api/random")
+        quote = f"*{text_data[0]['q']}*  -  ***{text_data[0]['a']}***"
+        return quote
+
+    # Function to send content to all the daily channels
+    async def send_dailies(self, task_embed: stoat.SendableEmbed):
+        # Fetch the list of enrolled channels to post daily content to
+        daily_channels = self.fetch_daily_channels()
+        # Send a meme to each of the daily channels
+        for channel_id in daily_channels:
+            daily_channel = self.bot.get_channel(channel_id)
+            if daily_channel is None:
+                daily_channel = await self.bot.fetch_channel(channel_id)
+            await daily_channel.send(embeds=[task_embed])
+
     async def daily_animal(self):
         # Gets daily animal
         try:
             # Create daily animal post
 
-            animal_picture = await get_animal(self.bot.client)
+            animal_picture = await self.get_animal(self.bot.client)
             animal = stoat.SendableEmbed(
                 title="😊\tHere's your cute animal of the day!\t😊",
                 color="blue",
@@ -126,7 +126,7 @@ class Fun(commands.Gear, name="Fun"):
             )
 
             # Get the animal embed and send it to each daily channel
-            await send_dailies(self.bot, animal)
+            await self.send_dailies(self.bot, animal)
 
         except Exception as e:
             print(f"The animal task error is: {e}")
@@ -134,7 +134,7 @@ class Fun(commands.Gear, name="Fun"):
     async def daily_joke(self):
         # Gets daily joke
         try:
-            joke, category = await get_joke(self.bot.client)
+            joke, category = await self.get_joke(self.bot.client)
             joke_embed = stoat.SendableEmbed(
                 title=f"{category} Joke",
                 description=joke,
@@ -142,7 +142,7 @@ class Fun(commands.Gear, name="Fun"):
             )
 
             # Get the joke embed and send it to each daily channel
-            await send_dailies(self.bot, joke_embed)
+            await self.send_dailies(self.bot, joke_embed)
 
         except Exception as e:
             print(f"The animal task error is: {e}")
@@ -150,17 +150,17 @@ class Fun(commands.Gear, name="Fun"):
     async def daily_meme(self):
         try:
             # Get the meme embed and send it to each daily channel
-            meme_embed: stoat.SendableEmbed = await get_meme(self.bot.client)
-            await send_dailies(self.bot, meme_embed)
+            meme_embed: stoat.SendableEmbed = await self.get_meme(self.bot.client)
+            await self.send_dailies(self.bot, meme_embed)
         except Exception as e:
             print(f"The meme task error is: {e}")
 
     async def daily_positivity(self):
         try:
             # Creates daily positivity post
-            advice = await get_advice(self.bot.client)
-            affirm = await get_affirmation(self.bot.client)
-            quote = await get_quote(self.bot.client)
+            advice = await self.get_advice(self.bot.client)
+            affirm = await self.get_affirmation(self.bot.client)
+            quote = await self.get_quote(self.bot.client)
             positivity = stoat.SendableEmbed(
                 title="😊\tHere's your reminder to stay positive today!\t😊",
                 description=f"Advice of the day:\t{advice}\nAffirmation of the day:\t{affirm}\n{quote}",
@@ -168,7 +168,7 @@ class Fun(commands.Gear, name="Fun"):
             )
 
             # Get the positivity embed and send it to each daily channel
-            await send_dailies(self.bot, positivity)
+            await self.send_dailies(self.bot, positivity)
 
         except Exception as e:
             print(f"The positivity task error is: {e}")
@@ -211,13 +211,13 @@ class Fun(commands.Gear, name="Fun"):
     @commands.command()
     async def animal(self, ctx: commands.Context):
         """Get a random animal picture"""
-        result = await get_animal(self.bot.client)
+        result = await self.get_animal(self.bot.client)
         await ctx.channel.send(attachments=[result])
 
     @commands.command()
     async def advice(self, ctx: commands.Context):
         """Get a random piece of advice"""
-        advice = await get_advice(self.bot.client)
+        advice = await self.get_advice(self.bot.client)
         embed = stoat.SendableEmbed(
             title=f"Advice for {ctx.author.display_name}:",
             description=f"{advice}.",
@@ -228,7 +228,7 @@ class Fun(commands.Gear, name="Fun"):
     @commands.command()
     async def affirmation(self, ctx: commands.Context):
         """Get a random affirmation"""
-        affirmation = get_affirmation()
+        affirmation = self.get_affirmation()
         embed = stoat.SendableEmbed(
             title=f"Affirmation for {ctx.author.display_name}:",
             description=f"{affirmation}.",
@@ -242,8 +242,8 @@ class Fun(commands.Gear, name="Fun"):
         self,
         ctx: commands.Context,
         embed_id: str,
-        title: str = None,
-        message: str = None,
+        title: str | None = None,
+        message: str | None = None,
     ):
         """Edit an embed, will only work if you are the author of the original embed. Type [] in your string to indicate any blank lines you want added to your message."""
         # Get the embed message from the message ID or return an error if it can't be found
@@ -265,9 +265,10 @@ class Fun(commands.Gear, name="Fun"):
             return await ctx.channel.send(
                 content=f"Embed with ID: {embed_id} should be updated", ephemeral=True
             )
-        except Exception:
+        except Exception as e:
             return await ctx.channel.send(
-                content=f"Unable to update embed with ID: {embed_id}", ephemeral=True
+                content=f"Unable to update embed with ID: {embed_id} due to {e}",
+                ephemeral=True,
             )
 
     @commands.command()
@@ -275,8 +276,8 @@ class Fun(commands.Gear, name="Fun"):
         self,
         ctx: commands.Context,
         *,
-        title: str = None,
-        message: str = None,
+        title: str | None = None,
+        message: str | None = None,
     ):
         """Create an embed. Type [] in your string to indicate any blank lines you want added to your message."""
         # Allow users to add newlines to their embed messages
@@ -341,14 +342,14 @@ class Fun(commands.Gear, name="Fun"):
     @commands.command()
     async def inspire(self, ctx: commands.Context):
         """Command to return an inspirational quote"""
-        quote = get_quote()
+        quote = self.get_quote()
         embed = stoat.SendableEmbed(title="", description=quote, color="blue")
         await ctx.channel.send(embeds=[embed])
 
     @commands.command()
     async def joke(self, ctx: commands.Context):
         """Gets a random joke"""
-        joke, category = get_joke()
+        joke, category = self.get_joke()
         joke_embed = stoat.SendableEmbed(
             title=f"{category} Joke",
             description=joke,
@@ -360,36 +361,37 @@ class Fun(commands.Gear, name="Fun"):
     @commands.command()
     async def meme(self, ctx: commands.Context):
         """Gets a random meme from r/memes, r/dankmemes, or r/me_irl"""
-        meme_post = get_meme()
+        meme_post = self.get_meme()
         await ctx.channel.send(embeds=[meme_post])
 
     @commands.command()
     async def youtube(self, ctx: commands.Context, *, message: str):
         """Search youtube for a video"""
-        query_string = parse.urlencode({"search_query": message})
-        html_content = request.urlopen("http://www.youtube.com/results?" + query_string)
-        search_content = html_content.read().decode()
-        search_results = findall(r"\/watch\?v=\w+", search_content)
-        await ctx.channel.send("https://www.youtube.com" + search_results[0])
+        videos_search = VideosSearch(message, limit=1)
+        video_results = await videos_search.next()["result"]
+        video_link = video_results[0]["link"]
+        await ctx.channel.send(video_link)
 
     @commands.command()
     @commands.has_permissions(manage_server=True)
-    async def set_daily_channel(self, ctx: commands.Context, channel: str):
+    async def set_daily_channel(self, ctx: commands.Context, channel_link: str):
         """Takes in a channel link/ID and sets it as the automated daily content channel for this server."""
 
         # Get the channel ID as an integer whether the user inputs a channel link or channel ID
-        daily_channel_id = int(channel.split("/")[-1])
-        # Prepares the new server & channel combination for this server
-        new_channel = {"server": ctx.channel.server_id, "channel": daily_channel_id}
-        # Updates the daily channel for the server or inserts it if one doesn't exist currently
-        db.daily_channels.replace_one(
-            {"server": ctx.channel.server_id}, new_channel, upsert=True
-        )
+        channel_id = int(channel_link.split("/")[-1])
+
+        with db.cursor() as cur:
+            cur.execute(
+                """INSERT INTO channels (server_id, category, channel_id) VALUES (%s, %s, %s) 
+                ON CONFLICT (server_id, category) DO UPDATE SET channel_id = EXCLUDED.channel_id""",
+                (ctx.server.id, "daily", channel_id),
+            )
+            db.commit()
 
         # Let users know where the updated channel is
-        updated_channel = ctx.channel.server.get_channel(ctx.channel.channel_id)
+        updated_channel = ctx.server.get_channel(ctx.channel.channel_id)
         if updated_channel:
-            await ctx.channel.send(
+            return await ctx.channel.send(
                 f"Daily content for this server will go to {updated_channel.name}."
             )
 
@@ -398,17 +400,21 @@ class Fun(commands.Gear, name="Fun"):
     async def remove_daily_channel(self, ctx: commands.Context):
         """Removes the automated daily content channel for this server, if it exists."""
 
-        # Removes the daily channel for the server if it exists
-        if db.daily_channels.find_one({"server": ctx.channel.server_id}):
-            db.daily_channels.delete_one({"server": ctx.channel.server_id})
-            await ctx.channel.send(
-                "Daily automated content for this server are stopped."
+        # Removes the daily channel if it exists
+        with db.cursor() as cur:
+            cur.execute(
+                "DELETE FROM channels WHERE (server_id, category) = (%s, %s) LIMIT 1",
+                (ctx.server.id, "daily"),
             )
-        # Lets the user know if there is no existing daily channel
-        else:
-            await ctx.channel.send(
-                "There is no daily automated content for this server."
-            )
+            db.commit()
+            if cur.rowcount == 0:
+                return await ctx.channel.send(
+                    "There is no daily content channel for this server."
+                )
+            else:
+                return await ctx.channel.send(
+                    "Daily content for this server is stopped."
+                )
 
 
 async def setup(bot: ChaosBot):
